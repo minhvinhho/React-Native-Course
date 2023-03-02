ed28801de6d7441752791a2cd09a2b9dbb14fb09
BAP-MINH <reynoldsho4078@gmail.com>
2023-03-02 09:26:42 +0700

    Working with Icons (Button Icons)

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/instructionText';
+import {Ionicons} from '@expo/vector-icons'
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -69,12 +70,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              -
+              <Ionicons name="md-remove" size={24} color={"white"} />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
-              +
+            <Ionicons name="md-add" size={24} color={"white"} />
             </PrimaryButton>
           </View>
         </View>
