72af8687dad11e171db0420ac4a1ad8d50db8a83
BAP-MINH <reynoldsho4078@gmail.com>
2023-03-02 11:10:08 +0700

    Finishing Touches

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if(currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -87,7 +87,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View tyle={styles.listContainer}>
       <FlatList
           data={guessRounds}
           renderItem={(itemData) => (
@@ -118,5 +118,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16
   },
 });
